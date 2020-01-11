7457ea2246a5fef30f142a7ab37f96874ad436f0
mf169 <mf169@hdm-stuttgart.de>
2020-01-11 11:47:19 +0100

    testReaderclass

--- src/test/java/mi/hdm_stuttgart/de/se1/testReader.java
+++ src/test/java/mi/hdm_stuttgart/de/se1/testReader.java
@@ -9,21 +9,23 @@ public class testReader {
     //TODO negativ Tests --> assertfalse
 
     public void testReader1() {
+        boolean condition = false;
         Reader reader = new Reader();
         Reader.init();
         Currency swissFranc = new Currency("Swiss franc", 0.7393440000);
         for (int i = 0; i < Reader.CurrencyList.length; i++) {
             if (swissFranc.equals(Reader.CurrencyList[i])){
-                Assert.assertTrue(true);
+                condition = true;
             }
         }
         Currency chileanPeso = new Currency("Chilean peso", 0.0009636630);
         for (int i = 0; i < Reader.CurrencyList.length; i++) {
             if (chileanPeso.equals(Reader.CurrencyList[i])){
-                //TODO set boolean true
-                Assert.assertTrue(true);
+                //TODO set boolean true --> done
+                condition = true;
             }
-        } //TODO asserttrue hier rein!
+        } //TODO asserttrue hier rein! --> done
+        Assert.assertTrue(condition == true);
     }
 
 }
